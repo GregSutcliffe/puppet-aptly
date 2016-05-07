73bc9a5e349186d7bba1e8f941d5ec8fb59d96e1
Greg Sutcliffe <greg.sutcliffe@gmail.com>
2016-05-07 21:19:07 +0100

    Extend API class to support a systemd service
    Also validate the enable_cli_and_http boolean

--- manifests/api.pp
+++ manifests/api.pp
@@ -8,6 +8,10 @@
 #   Ensure to pass on to service type
 #   Default: running
 #
+# [*type*]
+#   Type of service to deploy - upstart or systemd
+#   Default: upstart
+#
 # [*user*]
 #   User to run the service as.
 #   Default: root
@@ -21,7 +25,7 @@
 #   Default: ':8080'
 #
 # [*log*]
-#   Enable or disable Upstart logging.
+#   Enable or disable Upstart logging (logging is always enabled for systemd).
 #   Default: none
 #
 # [*enable_cli_and_http*]
@@ -30,22 +34,43 @@
 #
 class aptly::api (
   $ensure              = running,
+  $type                = 'upstart',
   $user                = 'root',
   $group               = 'root',
   $listen              = ':8080',
   $log                 = 'none',
   $enable_cli_and_http = false,
-  ) {
+) {
 
-    validate_re($ensure, ['^stopped|running$'], 'Valid values for $ensure: stopped, running')
+  validate_string($user, $group)
 
-    validate_string($user, $group)
+  validate_re($ensure, ['^stopped|running$'], 'Valid values for $ensure: stopped, running')
+  validate_re($type, ['^upstart|systemd$'], 'Valid values for $type: upstart, systemd')
+  validate_re($listen, ['^[0-9.]*:[0-9]+$'], 'Valid values for $listen: :port, <ip>:<port>')
+  validate_re($log, ['^none|log$'], 'Valid values for $log: none, log')
 
-    validate_re($listen, ['^[0-9.]*:[0-9]+$'], 'Valid values for $listen: :port, <ip>:<port>')
+  validate_bool($enable_cli_and_http)
 
-    validate_re($log, ['^none|log$'], 'Valid values for $log: none, log')
+  if $type == 'systemd' {
+    file { 'aptly-service':
+      path    => '/etc/systemd/system/aptly-api.service',
+      content => template('aptly/etc/aptly.systemd.erb'),
+    }
 
-    file{'aptly-upstart':
+    # Make sure systemctl is reloaded on file changes
+    exec { '/bin/systemctl daemon-reload':
+      refreshonly => true,
+      subscribe   => File['aptly-service'],
+      before      => Service['aptly-api'],
+    }
+
+    service{'aptly-api':
+      provider => 'systemd',
+      ensure   => $ensure,
+      enable   => true,
+    }
+  } else {
+    file { 'aptly-service':
       path    => '/etc/init/aptly-api.conf',
       content => template('aptly/etc/aptly.init.erb'),
     }
@@ -54,7 +79,8 @@ class aptly::api (
       ensure => $ensure,
       enable => true,
     }
+  }
 
-    File['aptly-upstart'] ~> Service['aptly-api']
+  File['aptly-service'] ~> Service['aptly-api']
 
 }
